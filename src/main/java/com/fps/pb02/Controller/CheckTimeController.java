1eca12ccbe97ad1dd4c780aa40d8354d3716ed69
dungdt <dungdt@GS-DungDT60.fsoft.fpt.vn>
2024-11-09 15:56:08 +0700

    rrrrrrrr
    rrrrrrrrr

--- src/main/java/com/fps/pb02/Controller/CheckTimeController.java
+++ src/main/java/com/fps/pb02/Controller/CheckTimeController.java
@@ -3,18 +3,15 @@ package com.fps.pb02.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import payload.ApiResponse;
+
 
 
 @RestController
 public class CheckTimeController {
-
-
-
 	@GetMapping("/liveserver")
-	public String  liveServer() {
-		return "live";
+	public ApiResponse  liveServer() {
+		return new ApiResponse(true, "live", "");
 	}
-
-
 
 }
--- src/main/java/com/fps/pb02/Service/ApiJob.java
+++ src/main/java/com/fps/pb02/Service/ApiJob.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  *
  */
@@ -16,12 +18,19 @@ public class ApiJob {
 
 	private final RestTemplate restTemplate = new RestTemplate();
 
-	@Scheduled(cron = "0 */2 * * * *", zone = "Asia/Ho_Chi_Minh") // Chạy mỗi 2 phút
+	@Scheduled(cron = "0 */1 * * * *", zone = "Asia/Ho_Chi_Minh") // Chạy mỗi 2 phút
 	public void runCallLive() {
 		String url = "https://checkin-time.onrender.com/liveserver"; // Địa chỉ của API
 		try {
 			String response = restTemplate.getForObject(url, String.class);
-			System.out.println("API response: " + response);
+
+			// Tạo ObjectMapper để phân tích JSON
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode rootNode = mapper.readTree(response);
+			// Trích xuất giá trị của trường "message"
+			String message = rootNode.path("message").asText();
+			System.out.println("Live server API: " + message);
+
 		} catch (Exception e) {
 			System.err.println("Error calling the live server API: " + e.getMessage());
 		}
